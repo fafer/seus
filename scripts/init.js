26b7743f2b21819be66e68c4fbc160602aee0b06
fafer <zhoucheng502@sina.com>
2020-07-03 14:44:47 +0800

    build: init project

---
+++ scripts/init.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+const cleanup = () => {
+  console.log('Cleaning up.');
+  cp.execSync(`git checkout -- packages/*/package.json`);
+};
+
+const handleExit = () => {
+  cleanup();
+  process.exit();
+};
+
+const handleError = e => {
+  console.error(e);
+  cleanup();
+  process.exit(1);
+};
+
+process.on('SIGINT', handleExit);
+process.on('uncaughtException', handleError);
+
+const rootDir = path.join(__dirname, '..');
+const packagesDir = path.join(rootDir, 'packages');
+const packagePathsByName = {};
+fs.readdirSync(packagesDir).forEach(name => {
+  const packageDir = path.join(packagesDir, name);
+  const packageJson = path.join(packageDir, 'package.json');
+  if (fs.existsSync(packageJson)) {
+    packagePathsByName[name] = packageDir;
+  }
+});
+Object.keys(packagePathsByName).forEach(name => {
+  const packageJson = path.join(packagePathsByName[name], 'package.json');
+  const json = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
+  Object.keys(packagePathsByName).forEach(otherName => {
+    if (json.dependencies && json.dependencies[otherName]) {
+      json.dependencies[otherName] = 'file:' + packagePathsByName[otherName];
+    }
+    if (json.devDependencies && json.devDependencies[otherName]) {
+      json.devDependencies[otherName] = 'file:' + packagePathsByName[otherName];
+    }
+    if (json.peerDependencies && json.peerDependencies[otherName]) {
+      json.peerDependencies[otherName] =
+        'file:' + packagePathsByName[otherName];
+    }
+    if (json.optionalDependencies && json.optionalDependencies[otherName]) {
+      json.optionalDependencies[otherName] =
+        'file:' + packagePathsByName[otherName];
+    }
+  });
+
+  fs.writeFileSync(packageJson, JSON.stringify(json, null, 2), 'utf8');
+});
+
+const scriptsFileName = cp
+  .execSync(`npm pack`, { cwd: path.join(packagesDir, 'seus-scripts') })
+  .toString()
+  .trim();
+const scriptsPath = path.join(packagesDir, 'seus-scripts', scriptsFileName);
+
+cp.execSync('yarn cache clean');
+
+const args = process.argv.slice(2);
+
+const scriptPath = path.join(packagesDir, 'seus-cli', '/lib/index.js');
+cp.execSync(
+  `node ${scriptPath} ${args.join(' ')} --scripts-version="${scriptsPath}"`,
+  {
+    cwd: rootDir,
+    stdio: 'inherit',
+  }
+);
+
+handleExit();
