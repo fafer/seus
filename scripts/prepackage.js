19927f21bd7f375ff69e30bec9c245e20472ca34
fafer <zhoucheng502@sina.com>
2020-07-07 14:59:38 +0800

    perf: mirror包安装

--- scripts/prepackage.js
+++ scripts/prepackage.js
@@ -1,4 +1,5 @@
 const spawn = require('cross-spawn');
+const execSync = require('child_process').execSync;
 const PLATFORM = {
   WINDOWS: 'WINDOWS',
   UNIX: 'UNIX'
@@ -15,10 +16,17 @@ function getPlatform() {
 }
 
 if(getPlatform() === PLATFORM.WINDOWS) {
-  spawn('npm', [
-    'i',
-    '-g',
-    '--registry=http://registry.npm.taobao.org',
-    'mirror-config-china'
-  ], { stdio: 'inherit' });
+  try {
+    const result = execSync('npm list mirror-config-china -g', { stdio: 'ignore' }).toString();
+    if(!result.includes('mirror-config-china')) {
+      spawn('npm', [
+        'i',
+        '-g',
+        '--registry=http://registry.npm.taobao.org',
+        'mirror-config-china'
+      ], { stdio: 'inherit' });
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
